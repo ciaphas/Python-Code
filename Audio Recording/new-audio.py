4a18febb4e21e4240cfcf594a32aa9c237b60443
James Carn <ciaphas@users.noreply.github.com>
2019-11-28 17:24:48 +0000

    Updated the New-Audio.py
    I have updated the app so that now you can specify the name of the lecuture and the time in munites

--- Audio Recording/new-audio.py
+++ Audio Recording/new-audio.py
@@ -6,9 +6,9 @@ from scipy.io.wavfile import write
 fs = 44100 #Sample RATE
 minutes=int(input("How Many Minutes would you like to record?"))
 rec_seconds=int(60)
-
+name=str(input("What Lecture is this?"))
 seconds=minutes*rec_seconds
 
 myrecording = sd.rec(int(seconds *fs), samplerate=fs, channels=2)
 sd.wait()
-write('output.wav', fs, myrecording)
+write(name+'.wav', fs, myrecording)
